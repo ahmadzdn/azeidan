f00d08f745b5117662e60775a592a2949fb87826
Ahmadzdn <ahmad.zeidan@outlook.fr>
2024-05-19 18:43:17 +0200

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ahmad-zeidan/portfolio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a3bead51fa051c8df0ea8bf78c37b8329300a71a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ahmad-zeidan/portfolio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a3bead51fa051c8df0ea8bf78c37b8329300a71a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
